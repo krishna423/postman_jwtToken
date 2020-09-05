3ed14ab05de2adad6c98adaa99f2ae5b1164bee7
krishna k maurya <krishna.maurya423@gmail.com>
2020-09-06 00:22:00 +0530

    parsing to new one integration

--- pre-request-script.js
+++ pre-request-script.js
@@ -1,5 +1,7 @@
 var CryptoJS = require("crypto-js");
 
+var requstKeysMap = new Map();
+
 function parseJwt(token) {
   var base64Header = token.split('.')[0]; 
   var base64Payload = token.split('.')[1];
@@ -23,11 +25,13 @@ function createJwt(header,payload,jwt_secret){
     console.log("new jwt token  :",jwtToken);
     pm.environment.set("jwt_token", jwtToken);
 }
+
 function encodingData(jsonData){
     var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(jsonData));
     var encodedData = base64url(stringifiedData);
     return encodedData;
 }
+
 function base64url(source) {
   // Encode in classical base64
   encodedSource = CryptoJS.enc.Base64.stringify(source);
@@ -39,54 +43,20 @@ function base64url(source) {
   return encodedSource;
 }
 
-
 function addSignature(unsignedToken,jwt_secret){
+    //var hmac = CryptoJS.createHmac('hmac-sha256', jwt_secret);
+    //return hmac.update(unsignedToken).digest('base64');
     return base64url(CryptoJS.HmacSHA256(unsignedToken, jwt_secret));
 }
 
-function jwtProcess (){
-    var jwt_secret =  pm.collectionVariables.get(JWT_SECRET);
-    var jwt_sample =  pm.collectionVariables.get(JWT_SAMPLE);
-    // console.log("jwt initial:- ",jwt_sample);
-    //console.log("jwt secret:- ",jwt_secret);
-    var [header,payload] = parseJwt(jwt_sample);
-    //console.log("parsed jwt:-  ",header,"    ",payload)
-    payload = createPayloadFromBody();
-    createJwt(header,payload,jwt_secret); 
+function createPayloadFromBody(jsonBody){
+    for(let key of Object.keys(jsonBody)) {
+        if(requstKeysMap.has(key)) {
+            jsonBody[key] = requstKeysMap.get(key);   
+        }    
+    }    
+    return jsonBody;
 }
-
-function createPayloadFromBody(){
-    var requestBody=pm.request.body;
-    //console.log(requestBody.options.raw.language);
-    if(requestBody.mode!="raw"){
-    console.error("script not configured for mode : ",requestBody.mode);
-    return;
-    }
-    if(requestBody.options.raw.language!="json"){
-    console.error("script not configured for language : ",requestBody.options.raw.language);
-    return;
-    }
-    var requestBodyPayload=JSON.parse(requestBody.raw);
-    return modifyPayload(requestBodyPayload);
-}
-
-
-// //---------------------------------config-------------------------------------------
-
-
-
-var JWT_SECRET = "ps_jwt_secret_key";
-var JWT_SAMPLE = "ps_jwt_sample";
-
-function modifyPayload(payload){
-    //add new field in json
-    payload['iss']=pm.collectionVariables.get("iss");
-    return payload;
-}
-//jwtProcess();
-
-
-var requstKeysMap = new Map();
 
 function parseRequestHeader(){
     var requestHeaderList = pm.request.headers.all();
@@ -147,9 +117,7 @@ function parseRawData(requestRawData){
     }
     var jsonData = JSON.parse(rawData);
     jsonObjectToMap(jsonData);
-
 }
-
 
 function parseRequestBody(){
     var requestBody = pm.request.body;
@@ -166,8 +134,6 @@ function parseRequestBody(){
         default :
             console.info("requestBody mode not match"); 
     }
-    console.log(requstKeysMap);
-
 }
 
 function createPrerequisiteMetadata(){
@@ -176,4 +142,25 @@ function createPrerequisiteMetadata(){
     parseRequestBody();
 }
 
-createPrerequisiteMetadata();
+function jwtProcess (){
+    var jwt_secret =  pm.collectionVariables.get(JWT_SECRET);
+    var jwt_sample =  pm.collectionVariables.get(JWT_SAMPLE);
+    // console.log("jwt initial:- ",jwt_sample);
+    //console.log("jwt secret:- ",jwt_secret);
+    createPrerequisiteMetadata();
+    
+    setTimeout(function(){
+        console.log("keys map;",requstKeysMap);
+        var [header,payload] = parseJwt(jwt_sample);
+        console.log("parsed jwt:-  ",header,"    ",payload)
+       // payload = createPayloadFromBody(payload);
+        createJwt(header,payload,jwt_secret); 
+    }, 100);
+    
+}
+
+//----------------------------------------config------------------------------------------------------------
+
+var JWT_SECRET = "ps_jwt_secret_key";
+var JWT_SAMPLE = "ps_jwt_sample";
+jwtProcess();
