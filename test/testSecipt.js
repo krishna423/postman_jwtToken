9a4bfae61bd82d7a190fae3330d71129a7b275d0
krishna k maurya <krishna.maurya423@gmail.com>
2020-09-12 01:23:16 +0530

    update sample test script

--- test/testSecipt.js
+++ test/testSecipt.js
@@ -3,11 +3,13 @@ var k=`({
         requstKeysMap : new Map(),
         func1()
         {
+            this.func3();
             console.log('This comes from func1',this.isSecretKeyBase64Encoded);
             
         },
         func2()
         {
+            this.func1();
             this.requstKeysMap.set('as','asaasa');
             console.log('This comes from func2',this.requstKeysMap)
         },
@@ -19,9 +21,5 @@ var k=`({
 
 var JWT_SECRET = "ps_jwt_secret_key";
 var JWT_SAMPLE = "ps_jwt_sample";
-//var t = pm.globals.get('jwt_script')
-//console.log(t);
 const obj = eval(k);
-obj.func1();
 obj.func2();
-obj.func3();
