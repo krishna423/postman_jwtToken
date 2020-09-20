d5b60ecab4b54fbf2bfd9dcac1609d0063e67b9d
krishna k maurya <krishna.maurya423@gmail.com>
2020-09-21 01:00:54 +0530

    working

--- dist/pre_request_min.js
+++ dist/pre_request_min.js
@@ -1,8 +1,4 @@
-
-//jwt_secret and jwt_sample should be collection variable
-var JWT_SECRET = "ps_jwt_secret_key";
-var JWT_SAMPLE = "ps_jwt_sample";
-var kk =`({
+({
     /**
      * @Author Krishna K. Maurya
      * @Project autoJWTCreation
@@ -228,14 +224,4 @@ var kk =`({
 
         }, 100);
     }
-})`;
-
-
-
-
-/** no need to change here */
-//var jwt_script = pm.globals.get("jwt_script");
-const obj = eval(kk);
-obj.jwtProcess();
-// console.log(jwt_script);
-// console.log( typeof pm.request.body.raw);
+})
